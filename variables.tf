6642e517bdd2f453fa2119afccdbcc2879cf0577
ItzikSdev <itziksavaia@gmail.com>
2024-10-05 13:04:39 +0300

    remove .pem in key

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "profile_name" {
 default = "default"
 }
 variable "instance_key" {
-default = "eksctl-my-cluster-nodegroup-ng1-public-d6:b9:ce:8c:bd:b7:48:d3:39:c2:32:4f:19:9c:0b:c4.pem"
+default = "eksctl-my-cluster-nodegroup-ng1-public-d6:b9:ce:8c:bd:b7:48:d3:39:c2:32:4f:19:9c:0b:c4"
 }
 variable "vpc_cidr" {
 default = "192.168.0.0/16"
